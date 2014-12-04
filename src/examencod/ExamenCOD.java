8e31d153836f4debf8e7e5d8f86cc04625886cc8
yperezmartinez <yperezmartinez@eniac12.danielcastelao.org>
2014-12-04 09:09:24 +0100

    metodoMio...Comprobacion

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -6,17 +6,17 @@ public class ExamenCOD {
 
     public static void main(String[] args) {
  int num = 11;
-            	if (metodoMio(num))
+            	if (Comprobacion(num))
 System.out.println("Es numero primo");
 else
  System.out.println("NO es numero primo");           	 
           	 
 for (int x=2;x<1000;x++) {
-if (metodoMio(x))
+if (Comprobacion(x))
 System.out.print(x + " ");                         	 
 }         	 
 	}
-public static boolean metodoMio(int variable){
+public static boolean Comprobacion(int variable){
 int j = 2;
 boolean aux=true;
 while ((aux) && (j!=variable)){
